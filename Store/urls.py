4d3f10ec11ba6024cbfc854a193efcf548991cee
KChisBao123 <kimchibao10@gmail.com>
2023-12-10 09:54:19 +0700

    12/10/2023 commit

--- Store/urls.py
+++ Store/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Store)
+    path('', views.Store),
+    path('cart/', views.Cart, name='cart'),
+    path('checkout/', views.Checkout, name='checkout'),
 ]
--- Store/views.py
+++ Store/views.py
@@ -4,3 +4,7 @@ from django.http import Http404
 # Create your views here.
 def Store(request):
     return render(request, 'store.html')
+def Cart(request):
+    return render(request, 'cart.html')
+def Checkout(request):
+    return render(request, 'checkout.html')
